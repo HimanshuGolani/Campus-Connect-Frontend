1b1973891a1a0f73d120eb28da45fc4a7e19fd17
Akash-akp <akashakp0037@gmail.com>
2025-02-23 09:44:44 +0530

    add public url for backend

--- src/chat/proxyService1.js
+++ src/chat/proxyService1.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const proxyService1 = axios.create({
-  baseURL: 'http://localhost:8000/', // Replace with your actual proxy server URL
+  // baseURL: 'http://localhost:8000/', 
+  baseURL: 'https://campusconnnectchatbackend.onrender.com/',
 });
 
 export default proxyService1;
